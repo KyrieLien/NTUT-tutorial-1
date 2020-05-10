855db975dad7bcbb091230cb32873f2ee23e11e3
KyrieLien <32617146+KyrieLien@users.noreply.github.com>
2020-05-10 09:57:08 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ class Params:
     PATH_TEST = './data/test.npy'
     PATH_WB = './data/wb_model.model'
 
-    B_SIZE = 1024
+    B_SIZE = 128
     INPUT_SIZE = 100
     HIDDEN_SIZE = 100
     OUTPUT_SIZE = 2
-    LR = 1e-1
+    LR = 2e-5
     EPOCH = 10
